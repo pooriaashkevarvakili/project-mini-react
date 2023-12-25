e8ed7e53a659e9933876bbe9aa13514609362cc7
pooria Vakili <pooriavakili09@gmail.com>
2023-12-25 10:29:39 +0330

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
-import Navbar from "../layouts/Navbar"
-export default function App() {
+import Head from "next/head";
+import React from 'react'
+
+const Home = () => {
     return (
-        <div>
+        <>
+            <Head>
+                <title>home</title>
+                <meta name="description" content="home" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" className="rounded-full" href="/images/meshki.ico" />	            </Head>
+            <main>
             
-        </div>
-    )
-}
+            </main>
+        </>
+    );
+};
+
+
+export default Home;
