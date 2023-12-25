5fc40685d3525a3244393c0704f78f9c49baf365
pooria Vakili <pooriavakili09@gmail.com>
2023-12-25 11:54:04 +0330

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,26 @@
 import Link from "next/link"
 import "../assets/styles/modules/globals.scss"
 
+import "../assets/styles/modules/css/style.css"
 export default function Custom404() {
     return (
-        <div className="flex w-full items-center justify-center h-screen">
-            <Link className="p-3 rounded-xl bg-black text-white" href="/">home</Link>
+        <div className="flex items-center  justify-center w-full h-screen">
+            <section className="error-page section">
+                <div className="container">
+                    <div className="flex">
+                        <div className="w-10/12  ">
 
-        </div>)
+                            <div className="error-inner">
+                                <h1>404<span>Oop's  sorry we can't find that page!</span></h1>
+
+                                <Link className="pl-52 flex items-center justify-center relative top-4  h-20 bg-blue-400 text-white rounded-xl " href="/">
+                                    <span className="relative right-24">Home</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
